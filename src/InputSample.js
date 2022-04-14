da369b4608222f34095136aeac0d1abefa8fae2a
MallangMallang <kcr92xb7vn@privaterelay.appleid.com>
2022-04-15 01:00:58 +0900

    Multiple Input States 여러개의 input 상태관리

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,40 @@
 import React, { useState } from "react";
 
 function InputSample() {
-  const [text, setText] = useState("");
-  const onChange = (e) => {
-    setText(e.target.value);
-  };
+  const [inputs, setInputs] = useState({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = inputs;
 
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    //Spread 문법으로 값을 재 할당 해줘야함 (불변성을 지켜야 한다.)
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
   const onReset = () => {
-    setText("");
+    setInputs({
+      name: "",
+      nickname: "",
+    });
   };
   return (
     <div>
       {/* 값이 바뀌고 input을 비우려면 value값을 설정 */}
-      <input onChange={onChange} value={text} />
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
         <b>값: </b>
-        {text}
+        {name} ({nickname})
       </div>
     </div>
   );

6bd179592a65d594266f1698ab4214c35a548422
Mallang-Mallang <gudwns1458@naver.com>
2022-04-15 18:59:06 +0900

    Implement onToggle

--- src/App.js
+++ src/App.js
@@ -23,16 +23,19 @@ function App() {
       id: 1,
       username: "Mallang",
       email: "mallang@gmail.com",
+      active: true,
     },
     {
       id: 2,
       username: "tester",
       email: "tester@gmail.com",
+      active: false,
     },
     {
       id: 3,
       username: "chj",
       email: "chj@gmail.com",
+      active: false,
     },
   ]);
 
@@ -54,13 +57,28 @@ function App() {
   };
 
   const onRemove = (id) => {
+    // user.id가 파라미터랑 일치하지 않는 원소만 추출해서 새로운 배열을 만듦
+    // = user.id가 id 인 것을 제거함
     setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
   };
 
   return (
     <>
-      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-      <UserList users={users} onRemove={onRemove} />
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,22 +1,36 @@
 import React from "react";
 
-function User({ user, onRemove }) {
+function User({ user, onRemove, onToggle }) {
   return (
     <div>
       <div>
-        <b>{user.username}</b> <span>{user.email}</span>
+        <b
+          style={{
+            cursor: "pointer",
+            color: user.active ? "green" : "black",
+          }}
+          onClick={() => onToggle(user.id)}
+        >
+          {user.username}
+        </b>{" "}
+        <span>{user.email}</span>
         <button onClick={() => onRemove(user.id)}>삭제</button>
       </div>
     </div>
   );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map((user) => (
         //key값을 정해줘야 렌더링 성능에 문제가 안됨. 값이 업데이트 될 때 참조를 하기 때문.
-        <User user={user} key={user.id} onRemove={onRemove} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );

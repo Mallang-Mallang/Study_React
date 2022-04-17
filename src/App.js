5313f58e22624d27c41fcc8f786c3d2d226809e4
MallangMallang <kcr92xb7vn@privaterelay.appleid.com>
2022-04-17 22:39:48 +0900

    chore: React.memo로 Component 최적화

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import { useRef, useState } from "react";
+import React, { useRef, useState, useMemo, useCallback } from "react";
 import "./App.css";
 import CreateUser from "./CreateUser";
 import UserList from "./UserList";
+
+function countActiveUsers(users) {
+  console.log("활성 사용자 수를 세는중...");
+  return users.filter((user) => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -10,13 +15,16 @@ function App() {
   });
   const { username, email } = inputs;
 
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs]
+  );
 
   const [users, setUsers] = useState([
     {
@@ -38,10 +46,9 @@ function App() {
       active: false,
     },
   ]);
-
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -54,22 +61,29 @@ function App() {
     });
     console.log(nextId.current);
     nextId.current += 1;
-  };
+  }, [users, username, email]);
 
-  const onRemove = (id) => {
-    // user.id가 파라미터랑 일치하지 않는 원소만 추출해서 새로운 배열을 만듦
-    // = user.id가 id 인 것을 제거함
-    setUsers(users.filter((user) => user.id !== id));
-  };
+  const onRemove = useCallback(
+    (id) => {
+      // user.id가 파라미터랑 일치하지 않는 원소만 추출해서 새로운 배열을 만듦
+      // = user.id가 id 인 것을 제거함
+      setUsers(users.filter((user) => user.id !== id));
+    },
+    [users]
+  );
 
-  const onToggle = (id) => {
-    setUsers(
-      users.map((user) =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  };
-
+  const onToggle = useCallback(
+    (id) => {
+      setUsers(
+        users.map((user) =>
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  );
+  //useMemo Hook을 사용하면 이전에 계산 한 값을 재사용(memorized)
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
       <CreateUser
@@ -79,8 +93,9 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성 사용자 수: {count}</div>
     </>
   );
 }
 
-export default App;
+export default React.memo(App);
--- src/UserList.js
+++ src/UserList.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function User({ user, onRemove, onToggle }) {
+const User = React.memo(function User({ user, onRemove, onToggle }) {
+  const { username, email, id, active } = user;
+  // useEffect(() => {
+  //   //컴포넌트가 마운트(처음 나타났을 때) 됐을 때 작업 처리
+  //   console.log("user값이 설정됨");
+  //   console.log(user);
+  //   return () => {
+  //     //컴포넌트가 언마운트(사라질 때) 됐을 때 작업 처리
+  //     console.log("user가 바뀌기 전");
+  //     console.log(user);
+  //   };
+  // }, [user]);
+  // //deps에 특정 값을 넣으면 처음 마운트 될 때도 호출이 되고, 업데이트 될 때도 호출이 됨.
+  //반대로 언마운트 될 때도 호출이 되고, 값이 바뀌기 직전에도 호출이 됨.
+
   return (
     <div>
       <div>
         <b
           style={{
             cursor: "pointer",
-            color: user.active ? "green" : "black",
+            color: active ? "green" : "black",
           }}
           onClick={() => onToggle(user.id)}
         >
-          {user.username}
-        </b>{" "}
-        <span>{user.email}</span>
-        <button onClick={() => onRemove(user.id)}>삭제</button>
+          {username}
+        </b>
+        &nbsp;
+        <span>{email}</span>
+        <button onClick={() => onRemove(id)}>삭제</button>
       </div>
     </div>
   );
-}
+});
 
 function UserList({ users, onRemove, onToggle }) {
   return (
@@ -36,4 +51,4 @@ function UserList({ users, onRemove, onToggle }) {
   );
 }
 
-export default UserList;
+export default React.memo(UserList);
